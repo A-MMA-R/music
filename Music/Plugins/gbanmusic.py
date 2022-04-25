356bdee68d088610a5e5be925474f9b2b64d7d18
A-MMA-R <103984735+A-MMA-R@users.noreply.github.com>
2022-04-25 16:04:51 +0200

    Update gbanmusic.py

--- Music/Plugins/gbanmusic.py
+++ Music/Plugins/gbanmusic.py
@@ -12,7 +12,7 @@ from pyrogram import filters
 from pyrogram.errors import FloodWait
 
 
-@app.on_message(filters.command("حظر ") & filters.user(OWNER))
+@app.on_message(filters.command("حظر") & filters.user(OWNER))
 async def ban_globally(_, message):
     if not message.reply_to_message:
         if len(message.command) < 2:
