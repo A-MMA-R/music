76c623a9505aab0660681c45bacc62f83f98ac39
A-MMA-R <103984735+A-MMA-R@users.noreply.github.com>
2022-04-30 11:00:41 +0200

    Update filters.py

--- Music/MusicUtilities/helpers/filters.py
+++ Music/MusicUtilities/helpers/filters.py
@@ -1,13 +1,13 @@
-from typing import List
-from typing import Union
-from pyrogram import filters
-from Music.config import COMMAND_PREFIXES
+from typing import List, Union
 
-other_filters = filters.group & ~ filters.edited & \
-    ~ filters.via_bot & ~ filters.forwarded
-other_filters2 = filters.private & ~ filters.edited \
-    & ~ filters.via_bot & ~ filters.forwarded
+from pyrogram import filters
+
+
+other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+other_filters2 = (
+    filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+)
 
 
 def command(commands: Union[str, List[str]]):
-    return filters.command(commands, COMMAND_PREFIXES)
+    return filters.command(commands, "")
