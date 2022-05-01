fbf27d0fe6a45637f5305b26151915ca1b1280e2
A-MMA-R <103984735+A-MMA-R@users.noreply.github.com>
2022-05-01 20:48:01 +0200

    Update pic.py

--- Music/Plugins/pic.py
+++ Music/Plugins/pic.py
@@ -1,10 +1,51 @@
 import asyncio
 from pyrogram import Client, filters
+
+from Music.config import GROUP, CHANNEL
+from Music import (
+    ASSID,
+    BOT_ID,
+    BOT_NAME,
+    BOT_USERNAME,
+    OWNER,
+    SUDOERS,
+    app,
+)
+
 from pyrogram.types import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
     Message,
 )
+
+DEV_BOT = getenv("DEV_BOT")
+BOTID = getenv("BOTID")
+DEVID = getenv("DEVID")
+DEV_BOT1 = getenv("DEV_BOT1")
+NAME_BOT = getenv("NAME_BOT")
+
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            # "contact",
+            # "dice",
+            # "poll",
+            # "location",
+            # "venue",
+            "sticker",
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                setattr(obj, "message_type", message_type)
+                return obj
+
 
 @app.on_message(command(["/pics"]) & filters.group)
 async def Khalid(client: Client, message: Message):
