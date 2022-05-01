d50bafb5a1a2728e4376f6afaa6ad471123852f8
A-MMA-R <103984735+A-MMA-R@users.noreply.github.com>
2022-05-01 13:39:41 +0200

    Update kt.py

--- Music/Plugins/kt.py
+++ Music/Plugins/kt.py
@@ -1276,7 +1276,7 @@ def searchMusic(c, m):
         m.reply("حدث خطا راسل المطور\n- @X_A_R3")
 
 @Client.on_message(command(["Kstop", f"Kstop@fi0nabot", "تعطيل الكت"]) & ~filters.edited)
-@authorized_users_only
+
 async def stop_filter(client: Client, message: Message):
     start = time()
     m_reply = await message.reply_text(
@@ -1291,7 +1291,7 @@ async def stop_filter(client: Client, message: Message):
 
     
 @Client.on_message(filters.command("فيونا اقفلي الكت", [".", ""]) & ~filters.edited)
-@authorized_users_only
+
 async def stop_filterr(client: Client, message: Message):
     start = time()
     m_reply = await message.reply_text(
@@ -1305,7 +1305,7 @@ async def stop_filterr(client: Client, message: Message):
     )
 
 @Client.on_message(command(["Kstart", f"Kstart@fi0nabot"]) & ~filters.edited)
-@authorized_users_only
+
 async def start_filter(client: Client, message: Message):
     start = time()
     m_reply = await message.reply_text(
@@ -1319,7 +1319,7 @@ async def start_filter(client: Client, message: Message):
     )
     
 @Client.on_message(filters.command("فيونا شغلي الكت", [".", ""]) & ~filters.edited)
-@authorized_users_only
+
 async def start_filterrr(client: Client, message: Message):
     start = time()
     m_reply = await message.reply_text(
