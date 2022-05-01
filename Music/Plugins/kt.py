9b06a743e090e3fa0bad13ee59e3850f4c8a03db
A-MMA-R <103984735+A-MMA-R@users.noreply.github.com>
2022-05-01 14:56:24 +0200

    Update kt.py

--- Music/Plugins/kt.py
+++ Music/Plugins/kt.py
@@ -1,6 +1,7 @@
 import os
 import random
 
+from time import time
 from datetime import datetime
 from Music.config import GROUP, CHANNEL
 from Music import (
@@ -40,6 +41,35 @@ from pyrogram.types import (
     InlineKeyboardMarkup,
     Message,
 )
+
+__major__ = 0
+__minor__ = 2
+__micro__ = 1
+
+__python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ("week", 60 * 60 * 24 * 7),
+    ("day", 60 * 60 * 24),
+    ("hour", 60 * 60),
+    ("min", 60),
+    ("sec", 1),
+)
+
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return "inf"
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else "s"))
+    return ", ".join(parts)
+
 
 @Client.on_message(filters.regex("^كت$"))
 def searchMusic(c, m):
