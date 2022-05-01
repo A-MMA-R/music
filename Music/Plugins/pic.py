6ef91888fe9baf02e73128c47eb97153153c0d9d
A-MMA-R <103984735+A-MMA-R@users.noreply.github.com>
2022-05-01 20:23:40 +0200

    Add files via upload

---
+++ Music/Plugins/pic.py
@@ -0,0 +1,27 @@
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButto  
+
+@app.on_message(
+    command(["صوري"])
+    & filters.group
+    & ~filters.edited
+)
+async ah start(client: Client, message: Message):
+    usr = await client.get_users(message.from_user.id)
+    name = usr.first_name
+    async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
+                    await message.reply_photo(photo.file_id,       caption=f"""نفس ألصوره كل مرة غير بق""", 
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{message.from_user.username}")
+                ],[
+                    InlineKeyboardButton(
+                        "╞. 𝒅𝒆𝒗 .╡", url=f"https://t.me/X_A_R3"),
+                ],
+            ]
+        ),
+    )
+
